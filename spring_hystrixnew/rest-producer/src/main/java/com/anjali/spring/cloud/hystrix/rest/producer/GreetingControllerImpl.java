6052f245677ae77c26d248b8ff3c2fdaba69eb6b
anjalisatraboina001 <26076934+anjalisatraboina001@users.noreply.github.com>
2019-07-19 11:01:15 -0600

    Update GreetingControllerImpl.java

--- spring_hystrixnew/rest-producer/src/main/java/com/anjali/spring/cloud/hystrix/rest/producer/GreetingControllerImpl.java
+++ spring_hystrixnew/rest-producer/src/main/java/com/anjali/spring/cloud/hystrix/rest/producer/GreetingControllerImpl.java
@@ -3,9 +3,6 @@ package com.anjali.spring.cloud.hystrix.rest.producer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by anjalisatraboina on 5/23/2017.
- */
 
 @RestController
 public class GreetingControllerImpl implements GreetingController {
